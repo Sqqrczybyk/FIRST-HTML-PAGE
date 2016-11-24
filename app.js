e0b097a7a537e054fac762ed212e49598d1dfc89
Ernest <ernesto1103@o2.pl>
2016-11-24 19:31:49 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+angular.module('main', [
+
+]) 
+.controller('MainCtrl', function($scope){
+	$scope.leftContent = [
+		{"id" : , "name": "O mnie"},
+		{"id" : , "name": "Zainteresowania"},
+		{"id" : , "name": "Curriculum vitae"},
+		{"id" : , "name": "Konkakt"},
+		{"id" : , "name": "lsfive"},
+	]
+});
